a3fce7a5ce545189516fa391735f3449f8e059af
Nathan Barnes <npbarnes@alaska.edu>
2015-05-21 18:03:47 -0800

    Plot real data alongside the simulation.

--- HW1.py
+++ HW1.py
@@ -20,4 +20,26 @@ for i in range(0,divisions-1):
 # This splits Temp into a list of times and a list of temperatures.
 simX, simY = zip(*Temp)
 plt.plot(simX,simY)
+
+Data = [
+        (0,83),
+        (1,77.7),
+        (2,75.1),
+        (3,73),
+        (4,71.1),
+        (5,69.4),
+        (6,67.8),
+        (7,66.4),
+        (8,64.7),
+        (9,63.4),
+        (10,62.1),
+        (11,61),
+        (12,59.9),
+        (13,58.7),
+        (14,57.8),
+        (15,56.6)
+    ]
+dataX, dataY = zip(*Data)
+plt.plot(dataX,dataY,"ro")
+
 plt.show()
