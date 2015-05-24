1be8565520f2369122cf72c12640f133e98eb93e
Nathan Barnes <npbarnes@alaska.edu>
2015-05-23 16:16:48 -0800

    Encapsulated Newton cooling into a function.

--- HW1.py
+++ HW1.py
@@ -3,9 +3,16 @@
 
 import matplotlib.pyplot as plt
 
+# Newton Cooling simulation using Euler method
+def NewtonCooling(initTemp, R, air, endTime, divisions):
+    ret = [(0,initTemp)]
+    dt = endTime/divisions
+    for i in range(0,divisions-1):
+        ret.append( ( i*dt, ret[i][1] + dt * (-R*(ret[i][1]-air)) ) )
+    return ret
+
 interval = 15  # Minutes
 divisions = 100
-dt = interval/divisions
 
 initialTemp = 83  # Degrees C
 TempAir = 30
@@ -30,10 +37,7 @@ Data = [
         (15,56.6)
     ]
 
-Temp = [(0,initialTemp)]
-
-for i in range(0,divisions-1):
-    Temp.append( ( i*dt, Temp[i][1] + dt * (-R*(Temp[i][1]-TempAir)) ) )
+Temp = NewtonCooling(initialTemp, R, TempAir, interval, divisions)
 
 # Data to be plotted is split into x and y components
 simX, simY = zip(*Temp)
