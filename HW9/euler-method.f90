a9ddd052270c5f8213422f36c51fd0e54d3014c4
Nathan Barnes <npbarnes@alaska.edu>
2015-06-03 11:55:05 -0800

    Lorentz force equation with normalized units.

--- HW9/euler-method.f90
+++ HW9/euler-method.f90
@@ -63,6 +63,15 @@ contains
         f = q*(E + (velocity .cross. B))
     end function lorentzForce
 
+    ! E and B in normalized units, i.e. qE/m -> E, and qB/m -> B
+    ! This will have units of acceleration, not force.
+    type(vector) elemental function normalizedLorentz(E,B,velocity) result(f)
+        type(vector), intent(in) :: E, B
+        type(vector), intent(in) :: velocity
+
+        f = (E + (velocity .cross. B))
+    end function normalizedLorentz
+
 end module Electromagnetism
 
 program euler
