1a3a929bcdbf8d1bbbc97ddd2eb6deefd36f3512
Nathan Barnes <npbarnes@alaska.edu>
2015-06-05 10:42:13 -0800

    The file is now specified in the command line.

--- HW9/plot.py
+++ HW9/plot.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 def getData(filename, column):
     with open(filename) as f:
@@ -12,8 +13,8 @@ def getData(filename, column):
     # This just ignores the last entry.
     return [row.split()[column] for row in data[:-1]]
 
-x = getData("./velocities",1)
-y = getData("./velocities",2)
+x = getData("./" + sys.argv[1],1)
+y = getData("./" + sys.argv[1],2)
 
 plt.plot(x,y)
 
